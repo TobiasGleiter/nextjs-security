012329da7ba28d53bd1e1b168ab275ff902bedd7
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-04-13 10:06:41 +0200

    feat: chain of responsibility

--- src/app/api/v1/order/route.ts
+++ src/app/api/v1/order/route.ts
@@ -1,20 +1,17 @@
 import { UserAuthenticationHandler } from "@/lib/handler/authentication.handler";
-import { ExamplenHandler } from "@/lib/handler/example.handler";
+import { ExampleHandler } from "@/lib/handler/example.handler";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, _context: any) {
   const userAuthenticationHandler = new UserAuthenticationHandler();
-  const exampleHandler = new ExamplenHandler();
+  const exampleHandler = new ExampleHandler();
 
   userAuthenticationHandler.setNext(exampleHandler);
 
   try {
     const json = await req.json();
-
-    // Security check is always necessary!
-    await userAuthenticationHandler.handle(json);
-
     // After this comment, all is validated and secure
+    await userAuthenticationHandler.handle(json);
 
     return NextResponse.json({ result: "All good!" }, { status: 201 });
   } catch (error) {
--- src/lib/handler/authentication.handler.ts
+++ src/lib/handler/authentication.handler.ts
@@ -2,19 +2,11 @@ import { AbstractRequestHandler } from "./request.handler";
 
 export class UserAuthenticationHandler extends AbstractRequestHandler {
   public async handle(requestJson: any): Promise<Error | null> {
-    const isCurrentSessionAvailable = await readCurrentSession();
-    if (!isCurrentSessionAvailable) {
+    const isCurrentSessionIsAvailable = true;
+    if (!isCurrentSessionIsAvailable) {
       throw Error("Forbidden");
     }
+
     return super.handle(requestJson);
   }
-}
-
-// Simulation of the session
-async function readCurrentSession(): Promise<null | boolean> {
-  const session = true;
-  if (!session) {
-    return null;
-  }
-  return session;
 }
