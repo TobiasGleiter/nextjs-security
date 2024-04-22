889dfe422d68471df41dc879300912662b458646
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-04-22 15:22:05 +0200

    chore: http header white list

--- src/app/docs/headers/page.tsx
+++ src/app/docs/headers/page.tsx
@@ -1,15 +1,33 @@
 "use client";
 
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
 export default function SecureHeaderPage() {
+  const [headerData, setHeaderData] = useState<Response>();
   const fetchData = async () => {
-    const data = await fetch("/api/v1/headers");
-    console.log(data);
+    const data = await fetch("/api/v1/headers", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await data.json();
+    setHeaderData(json);
   };
 
   return (
-    <div>
-      HeaderPage
-      <button onClick={fetchData}>FetchData</button>
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold">Headers</h1>
+      <button onClick={fetchData} className={cn(buttonVariants(), "w-[340px]")}>
+        Fetch data from /api/v1/cookies Endpoint
+      </button>
+      {headerData && (
+        <pre className="mt-2 w-[340px] rounded-md p-4 bg-zinc-100">
+          <code>{JSON.stringify(headerData, null, 2)}</code>
+        </pre>
+      )}
     </div>
   );
 }
